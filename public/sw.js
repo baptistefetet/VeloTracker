e108126c99c681cfbbd7adb90bc4e73eb7aaaa5f
Goshia-App <lyonmg110@gmail.com>
2026-03-04 21:03:32 +0100

    fix: single toggle button in header + remove SW activate event

--- public/sw.js
+++ public/sw.js
@@ -16,18 +16,6 @@ self.addEventListener('install', (event) => {
     );
 });
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames
-                    .filter((name) => name !== CACHE_NAME)
-                    .map((name) => caches.delete(name))
-            );
-        }).then(() => self.clients.claim())
-    );
-});
-
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         fetch(event.request)
